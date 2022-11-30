e575546633f4f35f68977c19ab8a873b84140dbb
Gopinadh-Kalakota <sgnreddy2003@gmail.com>
2022-11-30 16:52:51 +0530

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//import ClassComponent from './ClassComponent'
+import PropsExample from './PropsExample'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  return(
+    <section>
+      <PropsExample name="Gopinadh" age="19"/>
+      <PropsExample name="Phani" age="20"/>
+        {/*<p>Good afternoon</p>
+        <h1>heading tag</h1>
+        <img src="https://images.hindustantimes.com/img/2022/11/16/1600x900/dhoni_1668591485783_1668591486123_1668591486123.jpg"alt="image"width="10%"/>
+        <ClassComponent/>*/}
+    </section>
+  )
 }
-
-export default App;
+export default App
