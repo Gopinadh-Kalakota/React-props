d7a3a0dafbdbb5398adcc1240db5889b08023c8d
Gopinadh-Kalakota <sgnreddy2003@gmail.com>
2022-12-01 16:50:27 +0530

    props and events in react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 //import ClassComponent from './ClassComponent'
-import PropsExample from './PropsExample'
-
+//import PropsExample from './PropsExample'
+//import StateExample from './StateExample'
+//import FuncProps from './FuncProps'
+//import Demo from './Demo'
+import Events from './Events'
 function App(){
   return(
     <section>
+    <Events/>
+    {/*<Demo price="1st price"/>
+    FuncProps carname="rolls royce" year="1989"/>
+    <h1>Hello guru</h1>
       <PropsExample name="Gopinadh" age="19"/>
       <PropsExample name="Phani" age="20"/>
-        {/*<p>Good afternoon</p>
+        <p>Good afternoon</p>
         <h1>heading tag</h1>
         <img src="https://images.hindustantimes.com/img/2022/11/16/1600x900/dhoni_1668591485783_1668591486123_1668591486123.jpg"alt="image"width="10%"/>
         <ClassComponent/>*/}
